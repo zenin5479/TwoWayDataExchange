309125b2aebdf2830c51c87912d3407010c705bc
zenin5479 <zenin5479@gmail.com>
2026-04-06 10:00:49 +0300

    Update 1.1 Обновлен метод. Проведен рефакторинг

--- FormOne.Designer.cs
+++ FormOne.Designer.cs
@@ -28,19 +28,55 @@
       /// </summary>
       private void InitializeComponent()
       {
+         textBox1 = new System.Windows.Forms.TextBox();
+         label1 = new System.Windows.Forms.Label();
+         button1 = new System.Windows.Forms.Button();
          SuspendLayout();
+         // 
+         // textBox1
+         // 
+         textBox1.Location = new System.Drawing.Point(86, 109);
+         textBox1.Name = "textBox1";
+         textBox1.Size = new System.Drawing.Size(100, 23);
+         textBox1.TabIndex = 0;
+         // 
+         // label1
+         // 
+         label1.AutoSize = true;
+         label1.Location = new System.Drawing.Point(144, 198);
+         label1.Name = "label1";
+         label1.Size = new System.Drawing.Size(38, 15);
+         label1.TabIndex = 1;
+         label1.Text = "label1";
+         // 
+         // button1
+         // 
+         button1.Location = new System.Drawing.Point(105, 152);
+         button1.Name = "button1";
+         button1.Size = new System.Drawing.Size(75, 23);
+         button1.TabIndex = 2;
+         button1.Text = "button1";
+         button1.UseVisualStyleBackColor = true;
          // 
          // FormOne
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          ClientSize = new System.Drawing.Size(484, 361);
+         Controls.Add(button1);
+         Controls.Add(label1);
+         Controls.Add(textBox1);
          Name = "FormOne";
          StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
          Text = "Двусторонний обмен между Windows Forms и консолью ";
          ResumeLayout(false);
+         PerformLayout();
       }
 
       #endregion
+
+      private System.Windows.Forms.TextBox textBox1;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Button button1;
    }
 }
