ee6bbac61f6bdf76b241094ef427726885d7cedd
zenin5479 <zenin5479@gmail.com>
2026-04-29 14:14:33 +0300

    Update 6.2 Обновлен метод. Проведен рефакторинг

--- FormOne.cs
+++ FormOne.cs
@@ -57,7 +57,9 @@ namespace TwoWayDataExchange
             Console.WriteLine(@"Шаг {0}: умножаем на {1} -> текущий результат = {2}", i - 1, i, fact);
          }
 
-         Console.WriteLine("\nРезультат: {0}! = {1}\n", n, fact);
+         Console.WriteLine(@"
+Результат: {0}! = {1}
+", n, fact);
          return fact;
       }
 
